735b17276863d291504432bb860701fccb5e0bf8
Bereket Retta <bereket@etmsoftwareplc.com>
2022-01-23 11:39:59 +0300

    added some documentaion

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+// Import all variables
 import React from 'react';
 import {
   StyleSheet,
@@ -19,8 +20,11 @@ import MainVids from './assets/Feedback_Drills_Correct.mp4';
 import Voice from 'react-native-voice';
 import LottieView from 'lottie-react-native';
 
+// declare types of props 
+
 interface IProps {}
 
+// declare types of state 
 interface IState {
   playOrPause?: string;
   recognized?: string;
@@ -37,6 +41,7 @@ interface IState {
 export default class VoiceNative extends React.Component<IProps, IState> {
   constructor(props) {
     super(props);
+    // Define all the states
     this.state = {
       recognized: '',
       started: '',
@@ -49,34 +54,41 @@ export default class VoiceNative extends React.Component<IProps, IState> {
       checking: true,
     };
 
+    // Binding this to the voice recognition package
     Voice.onSpeechStart = this.onSpeechStart.bind(this);
     Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
     Voice.onSpeechResults = this.onSpeechResults.bind(this);
   }
 
+
   componentWillUnmount() {
+    // initialize all the listeners for the voice recognition
     Voice.destroy().then(Voice.removeAllListeners);
   }
 
   onSpeechStart(e) {
+    // invocked when the speech recognition start
     this.setState({
       started: '√',
     });
   }
 
   onSpeechRecognized(e) {
+    // invoked when the voice is recognised
     this.setState({
-      recognized: '√',
+      recognized: 'yes',
     });
   }
 
   onSpeechResults(e) {
+     // the speech is stored on the results array we defined on the state
     this.setState({
       results: e.value,
     });
   }
 
   async onSpeakEnds() {
+    // This is invoked when the speech ends
     this.setState({
       speak: false,
       success: true,
@@ -86,6 +98,7 @@ export default class VoiceNative extends React.Component<IProps, IState> {
   }
 
   async _startRecognition(e) {
+    // This function is invoked when the mic button is pressed to start the speech recognition
     this.setState({
       recognized: '',
       started: '',
@@ -101,13 +114,13 @@ export default class VoiceNative extends React.Component<IProps, IState> {
   }
 
   render() {
-    console.log('This.state,,,', this.state);
     setTimeout(() => {
-      this.state.checking === true ? 
-      this.setState({
-        first: true,
-        checking: false
-      }) : null
+      this.state.checking === true
+        ? this.setState({
+            first: true,
+            checking: false,
+          })
+        : null;
     }, 9000);
     setTimeout(() => {
       this.setState({
@@ -126,14 +139,7 @@ export default class VoiceNative extends React.Component<IProps, IState> {
             : require('./assets/Image32.png')
         }>
         <View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginHorizontal: 40,
-              marginTop: 70,
-            }}>
+          <View style={styles.mianConatiner}>
             <Close width={14} height={14} />
             <View style={{display: 'flex', flexDirection: 'row', marginTop: 5}}>
               <Linek style={{maginTop: 10}} />
@@ -141,24 +147,15 @@ export default class VoiceNative extends React.Component<IProps, IState> {
             </View>
             <MoreHorizontal width={14} height={14} />
           </View>
-          <View
-            style={{
-              // backgroundColor: '#2b2b2b',
-              width: 335,
-              height: 103,
-              borderRadius: 12,
-              // borderWidth: 1,
-              alignSelf: 'center',
-              marginTop: 20,
-            }}></View>
+          <View style={styles.contain}></View>
           <View style={{marginTop: -18}}>
             <View style={{opacity: 0}}>
               <MainImage />
             </View>
             <Video
               source={MainVids} // the video file
-              paused={this.state.paused} // make it start
-              // paused={true} // make it start
+              // paused={this.state.paused} // make it start
+              paused={true} // make it start
               style={styles.backgroundVideo} // any style you want
               repeat={true} // make it a loop
             />
@@ -172,28 +169,9 @@ export default class VoiceNative extends React.Component<IProps, IState> {
           {/* {this.state.speak ? : null} */}
           {this.state.first ? (
             <View>
-              <View
-                style={{
-                  backgroundColor: '#2b2b2b',
-                  width: 335,
-                  height: 103,
-                  borderRadius: 12,
-                  borderWidth: 1,
-                  alignSelf: 'center',
-                  marginTop: 20,
-                  position: 'absolute',
-                  top: -760,
-                }}>
+              <View style={styles.estupeContainer}>
                 <View>
-                  <Text
-                    style={{
-                      textAlign: 'center',
-                      color: '#fff',
-                      fontSize: 20,
-                      marginVertical: 5,
-                    }}>
-                    estupendo trabajo
-                  </Text>
+                  <Text style={styles.estupendo}>estupendo trabajo</Text>
                   <Text
                     style={{textAlign: 'center', color: '#fff', opacity: 0.5}}>
                     Great job!
@@ -204,53 +182,40 @@ export default class VoiceNative extends React.Component<IProps, IState> {
           ) : null}
           {this.state.first ? null : this.state.second ? (
             <View>
-              <View
-                style={{
-                  backgroundColor: '#fff',
-                  borderColor: '#fff',
-                  width: 335,
-                  height: 120,
-                  borderRadius: 12,
-                  borderWidth: 1,
-                  alignSelf: 'center',
-                  marginTop: -250,
-                  position: 'absolute',
-                }}>
+              <View style={styles.firstContainer}>
                 {this.state.success ? (
                   <View>
                     <View
-                      style={{
-                        width: 50,
-                        height: 50,
-                        borderRadius: 50,
-                        borderWidth: 1,
-                        alignSelf: 'center',
-                        marginTop: -30,
-                        backgroundColor: this.state.results[0] === 'Great job' ?  '#05AA1F' : 'red',
-                        borderColor: this.state.results[0] === 'Great job' ?  '#05AA1F' : 'red',
-                        position: 'absolute',
-                      }}>
-                      <Text
-                        style={{
-                          textAlign: 'center',
-                          marginTop: 6,
-                          fontSize: 30,
-                          color: '#fff',
-                          fontWeight: '500',
-                        }}>
-                        {this.state.results[0] === 'Great job' ?  'A+' : 'C-'}
+                      style={[
+                        {
+                          backgroundColor:
+                            this.state.results[0] === 'Great job'
+                              ? '#05AA1F'
+                              : 'red',
+                          borderColor:
+                            this.state.results[0] === 'Great job'
+                              ? '#05AA1F'
+                              : 'red',
+                        },
+                        styles.AplusContainer,
+                      ]}>
+                      <Text style={styles.Aplus}>
+                        {this.state.results[0] === 'Great job' ? 'A+' : 'C-'}
                       </Text>
                     </View>
                     <Text
-                      style={{
-                        textAlign: 'center',
-                        marginTop: 20,
-                        fontSize: 14,
-                        color: this.state.results[0] === 'Great job' ? '#05AA1F' : 'red',
-                        fontWeight: '500',
-                        // position: 'absolute'
-                      }}>
-                     {this.state.results[0] === 'Great job' ? 'WELL DONE!' : 'TRY AGAIN'}
+                      style={[
+                        {
+                          color:
+                            this.state.results[0] === 'Great job'
+                              ? '#05AA1F'
+                              : 'red',
+                        },
+                        styles.welldone,
+                      ]}>
+                      {this.state.results[0] === 'Great job'
+                        ? 'WELL DONE!'
+                        : 'TRY AGAIN'}
                     </Text>
                   </View>
                 ) : null}
@@ -258,20 +223,7 @@ export default class VoiceNative extends React.Component<IProps, IState> {
                   <View style={{marginTop: this.state.success ? 5 : 25}}>
                     <View style={{display: 'flex', flexDirection: 'row'}}>
                       <View style={{marginTop: 5, marginHorizontal: 8}}></View>
-                      <Text
-                        style={{
-                          textAlign: 'center',
-                          color: '#05AA1F',
-                          marginHorizontal: 5,
-                          fontSize: 20,
-                          borderColor: 'rgb(158, 233, 169)',
-                          backgroundColor: 'rgb(158, 233, 169)',
-                          fontWeight: '600',
-                          borderWidth: 1,
-                          paddingHorizontal: 5,
-                        }}>
-                        {this.state.results}
-                      </Text>
+                      <Text style={styles.results}>{this.state.results}</Text>
                     </View>
                   </View>
                 ) : (
@@ -280,40 +232,22 @@ export default class VoiceNative extends React.Component<IProps, IState> {
                       <View style={{marginTop: 5, marginHorizontal: 8}}>
                         <Speaker />
                       </View>
-                      <Text
-                        style={{
-                          textAlign: 'center',
-                          color: '#2b2b2b',
-                          marginHorizontal: 5,
-                          fontSize: 20,
-                        }}>
+                      <Text style={styles.greatJobSpanish}>
                         estupendo trabajo
                       </Text>
                     </View>
-                    <Text
-                      style={{
-                        textAlign: 'center',
-                        color: '#2b2b2b',
-                        opacity: 0.5,
-                        marginTop: 5,
-                      }}>
-                      Great job
-                    </Text>
+                    <Text style={styles.greatJob}>Great job</Text>
                   </View>
                 )}
 
                 {this.state.success ? (
                   <View
-                    style={{
-                      width: 50,
-                      height: 50,
-                      borderRadius: 17,
-                      // borderWidth: 1,
-                      alignSelf: 'center',
-                      marginTop: this.state.success ? 10 : 20,
-                      // backgroundColor: '#3038FF',
-                      // borderColor: '#3038FF',
-                    }}></View>
+                    style={[
+                      styles.micBorder,
+                      {
+                        marginTop: this.state.success ? 10 : 20,
+                      },
+                    ]}></View>
                 ) : (
                   <TouchableOpacity
                     onPress={
@@ -322,17 +256,10 @@ export default class VoiceNative extends React.Component<IProps, IState> {
                         : this._startRecognition.bind(this)
                     }>
                     <View
-                      style={{
-                        width: 50,
-                        height: 50,
-                        borderRadius: 17,
-                        borderWidth: 1,
-                        alignSelf: 'center',
-                        marginTop: this.state.success ? 10 : 20,
-                        backgroundColor: '#3038FF',
-                        borderColor: '#3038FF',
-                        zIndex: 9090909,
-                      }}>
+                      style={[
+                        styles.micContainer,
+                        {marginTop: this.state.success ? 10 : 20},
+                      ]}>
                       <View style={{alignSelf: 'center', marginTop: 13}}>
                         <Mic />
                       </View>
@@ -341,38 +268,12 @@ export default class VoiceNative extends React.Component<IProps, IState> {
                 )}
                 {this.state.success ? (
                   <TouchableOpacity>
-                    <View
-                      style={{
-                        width: 162.93,
-                        alignSelf: 'center',
-                        height: 42.74,
-                        borderWidth: 1,
-                        borderRadius: 15,
-                        backgroundColor: '#1E1F27',
-                      }}>
-                      <Text
-                        style={{
-                          color: '#fff',
-                          fontSize: 18,
-                          alignSelf: 'center',
-                          marginTop: 8,
-                        }}>
-                        Continue
-                      </Text>
+                    <View style={styles.continueContainer}>
+                      <Text style={styles.continue}>Continue</Text>
                     </View>
                   </TouchableOpacity>
                 ) : this.state.speak ? (
-                  <View
-                    style={{
-                      // width: 162.93,
-                      alignSelf: 'center',
-                      // height: 42.74,
-                      // borderWidth: 1,
-                      borderRadius: 15,
-                      position: 'absolute',
-                      bottom: 30,
-                      // backgroundColor: '#1E1F27',
-                    }}>
+                  <View style={styles.animations}>
                     <LottieView
                       source={require('./Recording.json')}
                       style={{width: 900, height: 200}}
@@ -381,15 +282,7 @@ export default class VoiceNative extends React.Component<IProps, IState> {
                     />
                   </View>
                 ) : (
-                  <View
-                    style={{
-                      width: 162.93,
-                      alignSelf: 'center',
-                      height: 42.74,
-                      // borderWidth: 1,
-                      borderRadius: 15,
-                      // backgroundColor: '#1E1F27',
-                    }}></View>
+                  <View style={styles.border}></View>
                 )}
               </View>
             </View>
@@ -399,6 +292,7 @@ export default class VoiceNative extends React.Component<IProps, IState> {
     );
   }
 }
+// The styles are all here
 const styles = StyleSheet.create({
   transcript: {
     textAlign: 'center',
@@ -438,6 +332,135 @@ const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
     opacity: 0.8,
+  },
+  border: {
+    width: 162.93,
+    alignSelf: 'center',
+    height: 42.74,
+    borderRadius: 15,
+  },
+  animations: {
+    alignSelf: 'center',
+    borderRadius: 15,
+    position: 'absolute',
+    bottom: 30,
+  },
+  continue: {
+    color: '#fff',
+    fontSize: 18,
+    alignSelf: 'center',
+    marginTop: 8,
+  },
+  continueContainer: {
+    width: 162.93,
+    alignSelf: 'center',
+    height: 42.74,
+    borderWidth: 1,
+    borderRadius: 15,
+    backgroundColor: '#1E1F27',
+  },
+  micContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 17,
+    borderWidth: 1,
+    alignSelf: 'center',
+    backgroundColor: '#3038FF',
+    borderColor: '#3038FF',
+    zIndex: 9090909,
+  },
+  micBorder: {
+    width: 50,
+    height: 50,
+    borderRadius: 17,
+    alignSelf: 'center',
+  },
+  greatJob: {
+    textAlign: 'center',
+    color: '#2b2b2b',
+    opacity: 0.5,
+    marginTop: 5,
+  },
+  greatJobSpanish: {
+    textAlign: 'center',
+    color: '#2b2b2b',
+    marginHorizontal: 5,
+    fontSize: 20,
+  },
+  results: {
+    textAlign: 'center',
+    color: '#05AA1F',
+    marginHorizontal: 5,
+    fontSize: 20,
+    borderColor: 'rgb(158, 233, 169)',
+    backgroundColor: 'rgb(158, 233, 169)',
+    fontWeight: '600',
+    borderWidth: 1,
+    paddingHorizontal: 5,
+  },
+  welldone: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  Aplus: {
+    textAlign: 'center',
+    marginTop: 6,
+    fontSize: 30,
+    color: '#fff',
+    fontWeight: '500',
+  },
+  AplusContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: -30,
+    position: 'absolute',
+  },
+  firstContainer: {
+    backgroundColor: '#fff',
+    borderColor: '#fff',
+    width: 335,
+    height: 120,
+    borderRadius: 12,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: -250,
+    position: 'absolute',
+  },
+  estupendo: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    marginVertical: 5,
+  },
+  estupeContainer: {
+    backgroundColor: '#2b2b2b',
+    width: 335,
+    height: 103,
+    borderRadius: 12,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 20,
+    position: 'absolute',
+    top: -760,
+  },
+  contain: {
+    width: 335,
+    height: 103,
+    borderRadius: 12,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  mianConatiner: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 70,
   },
 });
 
